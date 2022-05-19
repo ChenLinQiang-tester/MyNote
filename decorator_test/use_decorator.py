1a29a0ee9bbf82a7d3052c97417a68774c71f2e1
ChenLinQiang-tester <765970683@qq.com>
2022-05-19 11:36:32 +0800

    Default Changelist

--- decorator_test/use_decorator.py
+++ decorator_test/use_decorator.py
@@ -1,3 +1,23 @@
-import time
+import functools
 
-print(time.time())
+
+def log(text):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            print('%s %s():' % (text, func.__name__))
+            return func(*args, **kw)
+        return wrapper
+    return decorator
+
+
+@log('execute')
+def now(s):
+    print('2015-3-25'+s)
+
+
+if __name__ == '__main__':
+    now("时间")
+
+
+
